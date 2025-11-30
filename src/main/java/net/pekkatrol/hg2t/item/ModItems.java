ad891da6a0aeb5fe3c6b9c3df1fdae351290299a
AlessandroParis <alessandro1.paris@epitech.eu>
2025-11-30 12:06:41 +0100

    [+] add assets, model, lang, textures, and add the banana sprite and item

---
+++ src/main/java/net/pekkatrol/hg2t/item/ModItems.java
@@ -0,0 +1,21 @@
+package net.pekkatrol.hg2t.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.pekkatrol.hg2t.HG2Tomato;
+import org.spongepowered.asm.mixin.extensibility.IActivityContext;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, HG2Tomato.MOD_ID);
+
+public static final RegistryObject<Item> BANANA = ITEMS.register("banana",
+        () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
